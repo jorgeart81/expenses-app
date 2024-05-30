3179566e181467e8a690ae4f428b471cec9d3d10
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-05-29 21:48:10 -0500

    Install PreCompose

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,17 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import moe.tlaster.precompose.PreComposeApp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    PreComposeApp {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text("Curso Android Multiplatform")
+        }
+    }
+}
