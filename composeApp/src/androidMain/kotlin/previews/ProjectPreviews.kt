961211cd582a9440c92f15873f25f077fc99457d
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-05-30 19:49:42 -0500

    Create AllExpensesHeader composable

--- composeApp/src/androidMain/kotlin/previews/ProjectPreviews.kt
+++ composeApp/src/androidMain/kotlin/previews/ProjectPreviews.kt
@@ -2,10 +2,17 @@ package previews
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import ui.AllExpensesHeader
 import ui.ExpensesTotalHeader
 
 @Preview(showBackground = true)
 @Composable
 private fun ExpensesTotalHeaderPrev() {
     ExpensesTotalHeader(total = 10.0)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AllExpensesHeaderPrev() {
+    AllExpensesHeader()
 }
--- composeApp/src/commonMain/kotlin/ui/ExpensesScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/ExpensesScreen.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -28,11 +31,12 @@ fun ExpensesScreen(modifier: Modifier = Modifier) {
     val colors = getColorsTheme()
 
     LazyColumn(
-        modifier = Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         stickyHeader {
             Column(modifier = Modifier.background(colors.backgroundColor)) {
+                ExpensesTotalHeader(1028.9)
+                AllExpensesHeader()
             }
         }
         items(emptyList<String>()) {}
@@ -42,21 +46,43 @@ fun ExpensesScreen(modifier: Modifier = Modifier) {
 @Composable
 fun ExpensesTotalHeader(total: Double) {
     Card(
-        shape = RoundedCornerShape(30),
-        backgroundColor = Color.Black,
-        elevation = 5.dp
+        shape = RoundedCornerShape(30), backgroundColor = Color.Black, elevation = 5.dp
     ) {
         Box(
             modifier = Modifier.fillMaxWidth().height(130.dp).padding(16.dp),
             contentAlignment = Alignment.CenterStart
         ) {
             Text(
-                "$$total",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.ExtraBold,
-                color = Color.White
+                "$$total", fontSize = 30.sp, fontWeight = FontWeight.ExtraBold, color = Color.White
             )
             Text("USD", color = Color.Gray, modifier = Modifier.align(Alignment.CenterEnd))
+        }
+    }
+}
+
+@Composable
+fun AllExpensesHeader() {
+    val colors = getColorsTheme()
+
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            "All Expenses",
+            modifier = Modifier.weight(1f),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 20.sp,
+            color = colors.textColor
+        )
+
+        Button(
+            shape = RoundedCornerShape(50),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
+            onClick = {/*TODO: onClick*/ },
+            enabled = false
+        ) {
+            Text("View All")
         }
     }
 }
