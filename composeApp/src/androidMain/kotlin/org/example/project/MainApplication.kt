180a6bace2e1401a8cbfa634c1227b85cea29342
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-07-11 16:42:35 -0500

    Koin setup on IOS

--- composeApp/src/androidMain/kotlin/org/example/project/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainApplication.kt
@@ -1,6 +1,7 @@
 package org.example.project
 
 import android.app.Application
+import di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -12,8 +13,7 @@ class MainApplication : Application() {
         startKoin {
             androidContext(this@MainApplication)
             androidLogger()
-            // modules()
-
+            modules(appModule())
         }
     }
 }
